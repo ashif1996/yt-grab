cd71204d59748da67bc671317dced1c402f63aae
Ashif Hassan <ashifhassan1996@gmail.com>
2024-12-02 12:17:27 +0530

    Require necessary modules and set view engine and layouts

--- app.js
+++ app.js
@@ -1,10 +1,46 @@
 require("dotenv").config();
 
+const path = require("path");
+
 const express = require("express");
+const session = require("express-session");
+const flash = require("connect-flash");
+const expressLayouts = require("express-ejs-layouts");
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+}));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(expressLayouts);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(flash());
+
+const indexRoutes = require("./routes/indexRoutes");
+
+app.use("/", indexRoutes);
+
+app.use((req, res, next) => {
+    const locals = { title: "404 | Page Not Found!" };
+    return res.status(404).render("404", {
+        locals,
+        layout: "layouts/errorLayout",
+    });
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    const locals = { title: "500 | Internal Server Error!" };
+    return res.status(500).render("internalError", {
+        locals,
+        layout: "layouts/errorLayout",
+    });
 });
 
 const PORT = process.env.PORT || 4000;
