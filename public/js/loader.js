93ca86639f17ee6ffd03820ea08c01a1d90dd234
Ashif Hassan <ashifhassan1996@gmail.com>
2024-12-05 14:45:40 +0530

    Include event listener to ensure that the loader is hidden when navigating back to the page

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,9 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
     const loader = document.getElementById("loader");
-    loader.classList.add("hidden");
+    if (loader) {
+        loader.classList.add("hidden"); // Hide loader after page load
+    }
 });
 
+// Ensuring the loader is hidden when navigating back to the page
+window.addEventListener("pageshow", () => {
+    const loader = document.getElementById("loader");
+    if (loader) {
+        loader.classList.add("hidden");
+    }
+});
+
+// Show the loader when navigating away
 window.addEventListener("beforeunload", () => {
     const loader = document.getElementById("loader");
-    loader.classList.remove("hidden");
+    if (loader) {
+        loader.classList.remove("hidden");
+    }
 });
