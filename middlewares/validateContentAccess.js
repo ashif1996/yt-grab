b3875c2633a38869b9bdd6cac77e32a826d79f49
Ashif Hassan <ashifhassan1996@gmail.com>
2024-12-08 00:28:10 +0530

    Add utility function to show flash messages

--- middlewares/validateContentAccess.js
+++ middlewares/validateContentAccess.js
@@ -2,25 +2,46 @@ const httpStatusCodes = require("../utils/httpStatusCodes");
 
 const validateYouTubeVideoSession = (req, res, next) => {
     if (!req.session.videoDetails) {
-        req.flash("error", "Invalid access. Please enter a valid YouTube video URL.");
-        return res.redirect(httpStatusCodes.BAD_REQUEST, "/");
+        return showFlashMessages({
+            req,
+            res,
+            type: "error",
+            message: "Invalid access. Please enter a valid YouTube video URL.",
+            status: httpStatusCodes.BAD_REQUEST,
+            redirectUrl: "/",
+        });
     }
+
     next();
 };
 
 const validateYouTubeShortsSession = (req, res, next) => {
     if (!req.session.shortsDetails) {
-        req.flash("error", "Invalid access. Please enter a valid YouTube Shorts URL.");
-        return res.redirect(httpStatusCodes.BAD_REQUEST, "/");
+        return showFlashMessages({
+            req,
+            res,
+            type: "error",
+            message: "Invalid access. Please enter a valid YouTube Shorts URL.",
+            status: httpStatusCodes.BAD_REQUEST,
+            redirectUrl: "/",
+        });
     }
+
     next();
 };
 
 const validateCommunityPostSession = (req, res, next) => {
     if (!req.session.communityPostDetails) {
-        req.flash("error", "Invalid access. Please enter a valid Community Post URL.");
-        return res.redirect(httpStatusCodes.BAD_REQUEST, "/");
+        return showFlashMessages({
+            req,
+            res,
+            type: "error",
+            message: "Invalid access. Please enter a valid Community Post URL.",
+            status: httpStatusCodes.BAD_REQUEST,
+            redirectUrl: "/",
+        });
     }
+    
     next();
 };
 
