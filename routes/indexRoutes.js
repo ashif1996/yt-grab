98474fcc7bb047db5a4a3eb460275a4c679dae1d
Ashif Hassan <ashifhassan1996@gmail.com>
2024-12-08 19:35:26 +0530

    Add middleware to thumbnail routes

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -15,11 +15,11 @@ router.post("/process-url", indexControllers.processUrl);
 
 router.get("/yt-video", validateYouTubeVideoSession, indexControllers.getVideo);
 router.get("/yt-video/thumbnail", validateYouTubeVideoSession, indexControllers.getThumbnail);
-router.get("/yt-video/thumbnail/download-thumbnail", indexControllers.downloadThumbnail);
+router.get("/yt-video/thumbnail/download-thumbnail", validateYouTubeVideoSession, indexControllers.downloadThumbnail);
 router.get("/yt-shorts", validateYouTubeShortsSession, indexControllers.getShorts);
 
 router.get("/yt-community-post", validateCommunityPostSession, indexControllers.getCommunityPost);
-router.get("/yt-community-post/download-community-post/", indexControllers.downloadCommunityPost);
+router.get("/yt-community-post/download-community-post/", validateCommunityPostSession, indexControllers.downloadCommunityPost);
 
 router.get("/contact", indexControllers.getContact);
 router.post("/contact/send-email", indexControllers.processSendMail);
